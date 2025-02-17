135501e1c32c05f1cd80b78815a670c853c1b6a8
yangchenguang <yangchenguang@fpwis.com>
2025-02-17 10:05:24 +0800

    feat: 修改字体文件，bcryptjs更新

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -2,5 +2,6 @@ import { ZCOOL_XiaoWei } from 'next/font/google';
 
 export const zcoolXiaoWei = ZCOOL_XiaoWei({
     weight: '400',
-    subsets: ['latin']
+    subsets: ['latin'],
+    display: "swap",
 });
